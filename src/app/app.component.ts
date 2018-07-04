fe9498d0d851dae25b9d1149a221ce9b9c6334af
Marcin Jaroszyński <cinek_@poczta.onet.pl>
2018-07-04 23:20:01 +0200

    Edit task in progress...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Task } from './task';
 export class AppComponent {
   title = 'app';
   tasks: Task[];
+  mode: string;
   @Input() titleTask: string;
 
   constructor(private taskService: TaskService) {}
@@ -64,8 +65,22 @@ export class AppComponent {
     });
   }
 
+  editMode(): void {
+    this.mode = 'edit';
+  }
+
+  edit(newTitle): void {
+    console.log('New Title: ', newTitle);
+    this.mode = "read";
+  }
+
+  isReadMode(): boolean {
+    return this.mode == 'read' ? true : false;
+  }
+
   ngOnInit() {
   	this.tasks = [];
+    this.mode = 'read';
   	this.getTasks();
   }
 }
