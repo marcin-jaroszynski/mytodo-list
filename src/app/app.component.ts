927efec17ccb692811d57466e9991511f3c90b1b
Marcin Jaroszyński <marcin.jaroszynski@intive.com>
2018-07-24 16:55:52 +0200

    Login page in progress...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { TaskService } from './task.service';
-import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -9,79 +7,4 @@ import { Task } from './task';
 })
 export class AppComponent {
   title = 'app';
-  tasks: Task[];
-  @Input() typeTask: string;
-  @Input() titleTask: string;
-
-  constructor(private taskService: TaskService) {}
-
-  getTasks(): void {
-  	this.taskService.getList().subscribe((response) => { 
-  		if (true === response['success']) {
-  			for (let i = 0; i < response['tasks'].length; i++) {
-		  		let newTask = new Task.Builder()
-		  							  .setId(response['tasks'][i]._id)
-		  							  .setTitle(response['tasks'][i].title)
-		  							  .setStatus(response['tasks'][i].status)
-                      .setDateCreated(response['tasks'][i].created_date)
-		  							  .setDateFinished(response['tasks'][i].finished_date)
-		  							  .build();
-		  		this.tasks.push(newTask);
-		  	}
-	  	}
-  	});
-  }
-
-  addTask(): void {
-  	if (!this.titleTask.trim()) {
-      alert('Title of task cannot be empty!');
-      this.titleTask = '';
-      return;
-    }
-		this.taskService.addTask(this.titleTask.trim()).subscribe(response => {
-			if (true === response['success']) {
-				let newTask = new Task.Builder()
-	  							  .setId(response['task']._id)
-	  							  .setTitle(response['task'].title)
-	  							  .setStatus(response['task'].status)
-	  							  .setDateCreated(response['task'].created_date)
-	  							  .build();
-	  		this.tasks.push(newTask);
-	  		this.titleTask = '';
-			}
-		});
-  }
-
-  markTaskAsFinished(id: string): void {
-    this.taskService.markAsFinished(id).subscribe(response => {
-      if (true === response['success']) {
-        for (let i = 0; i < this.tasks.length; i++) {
-          if (id == this.tasks[i].id) {
-            this.tasks[i].date_finished = response['finished_date'];
-            this.tasks[i].status = 'finished';
-            break;
-          }
-        }
-      }
-    });
-  }
-
-  removeTask(id: string): void {
-    this.taskService.removeTask(id).subscribe(response => {
-      if (true === response['success']) {
-        for (let i = 0; i < this.tasks.length; i++) {
-          if (id == this.tasks[i].id) {
-            this.tasks.splice(i, 1);
-            break;
-          }
-        }
-      }
-    });
-  }
-
-  ngOnInit() {
-  	this.tasks = [];
-    this.typeTask = 'all';
-  	this.getTasks();
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from "@angular/material";
+import { MatDialogModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { TaskService } from './task.service';
 import { TaskComponent } from './task/task.component';
 import { PopupRemoveTaskComponent } from './popup-remove-task/popup-remove-task.component';
 import { FilterTaskPipe } from './filter-task.pipe';
-
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
     PopupRemoveTaskComponent,
-    FilterTaskPipe
+    FilterTaskPipe,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +32,7 @@ import { FilterTaskPipe } from './filter-task.pipe';
     MatDialogModule,
     MatButtonModule,
     BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [TaskService],
   bootstrap: [AppComponent],
